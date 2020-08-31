daaa0e663f8b493c4f696ffdc8cf0523f1d2382e
Punmanat <60070053@kmitl.ac.th>
2020-08-31 22:47:12 +0700

    update-delete profile

--- backend/patient-profile-service/src/router/patient.js
+++ backend/patient-profile-service/src/router/patient.js
@@ -10,6 +10,7 @@ const {
 } = require("../utils/ipfsFunction");
 const auth = require("../middleware/auth");
 
+// Create profile
 router.post("/", auth, async (req, res) => {
   try {
     const { instance } = await createInstance("http://127.0.0.1:22000");
@@ -30,6 +31,7 @@ router.post("/", auth, async (req, res) => {
   }
 });
 
+// get profile
 router.get("/", auth, async (req, res) => {
   try {
     const { instance } = await createInstance("http://127.0.0.1:22000");
@@ -39,16 +41,74 @@ router.get("/", auth, async (req, res) => {
       gas: "900000",
       gasPrice: "0",
     };
+    const valid = await instance.checkValid(
+      req.user.walletAddress,
+      options
+    );
+    if(!valid){
+      return res.status(404).send({error:"profile not found!"})
+    }
     const profile_byte32 = await instance.getProfile(
       req.user.walletAddress,
       options
     );
     const profile_ipfs = getIpfsHashFromBytes32(profile_byte32);
-    const data = await readDocuments(profile_ipfs);
+    let data = await readDocuments(profile_ipfs);
+    data = JSON.parse(data)
     res.send({ data });
   } catch (error) {
     console.log({ error });
     res.status(401).send({ error: "Please authenticate" });
   }
 });
+
+//update profile
+router.put('/', auth, async(req,res)=>{
+  try {
+    const { instance } = await createInstance("http://127.0.0.1:22000");
+    const options = {
+      from: req.user.walletAddress,
+      privateFor: ["BULeR8JyUWhiuuCMU/HLA0Q5pzkYT+cHII3ZKBey3Bo="],
+      gas: "900000",
+      gasPrice: "0",
+    };
+    const valid = await instance.checkValid(
+      req.user.walletAddress,
+      options
+    );
+    if(!valid){
+      return res.status(404).send({error:"profile not found!"})
+    }
+    const revision = parseInt(await instance.getRevision(req.user.walletAddress))
+    const profile = JSON.stringify({
+      ...req.body,
+      revision: revision + 1,
+    });
+    const cid = await addDocuments(profile);
+    const byte32_cid = getBytes32FromIpfsHash(cid);
+    const data = instance.contract.methods
+      .setProfile(req.user.walletAddress, byte32_cid, revision+1)
+      .encodeABI();
+    const receipt = await sendTransaction(instance.address, data, req.token);
+    res.send(receipt.data);
+  } catch (error) {
+    console.log({ error });
+    res.status(401).send({ error: "Please authenticate" });
+  }
+})
+
+//delete profile
+router.delete('/', auth, async(req,res)=>{
+  try {
+    const { instance } = await createInstance("http://127.0.0.1:22000");
+    const data = instance.contract.methods
+      .removeProfile(req.user.walletAddress)
+      .encodeABI();
+    const receipt = await sendTransaction(instance.address, data, req.token);
+    res.send(receipt.data);
+  } catch (error) {
+    console.log({ error });
+    res.status(401).send({ error: "Please authenticate" });
+  }
+})
 module.exports = router;
