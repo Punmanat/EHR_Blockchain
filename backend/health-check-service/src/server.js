cab8eed06097c7c81472d01ab972085c2a278621
Punmanat <60070053@kmitl.ac.th>
2021-02-15 21:55:04 +0700

    change cors

--- backend/health-check-service/src/server.js
+++ backend/health-check-service/src/server.js
@@ -7,19 +7,17 @@ const port = process.env.PORT | 4000;
 const encounterRouter = require("./router/encounter");
 const observationRouter = require("./router/observation");
 const allergyIntoleranceRouter = require("./router/allergyIntolerance");
-// const practitionerRouter = require("./router/practitioner");
 const cors = require("cors");
 
 // use to convert all object to json
 app.use(Express.json());
+// allows restricted resources on a web page to
+// be requested from another domain outside the domain
+app.use(cors());
 // add router to middleware
 app.use("/api/health/encounter", encounterRouter);
 app.use("/api/health/observation", observationRouter);
 app.use("/api/health/allergyIntolerance", allergyIntoleranceRouter);
-
-// allows restricted resources on a web page to
-// be requested from another domain outside the domain
-app.use(cors());
 
 // starting server
 app.listen(port, () => {
